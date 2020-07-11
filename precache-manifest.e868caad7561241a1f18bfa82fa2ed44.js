9508face7e0e29b7bcdaaf12ea761613066840fe
paulords95 <paulords95@gmail.com>
2020-07-11 12:42:44 -0300

    Updates

---
+++ precache-manifest.e868caad7561241a1f18bfa82fa2ed44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8fe9d296131f330cbbf12e2914c933",
+    "url": "/WhereIsWaldo/index.html"
+  },
+  {
+    "revision": "42bcbb7d24bb6a472ed7",
+    "url": "/WhereIsWaldo/static/css/main.3b9aaa0e.chunk.css"
+  },
+  {
+    "revision": "db8c966f7e7e50627d35",
+    "url": "/WhereIsWaldo/static/js/2.906c5573.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/WhereIsWaldo/static/js/2.906c5573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42bcbb7d24bb6a472ed7",
+    "url": "/WhereIsWaldo/static/js/main.b7ed21c9.chunk.js"
+  },
+  {
+    "revision": "99cd1012678ce75a7e53",
+    "url": "/WhereIsWaldo/static/js/runtime-main.12fb8c0c.js"
+  }
+]);
